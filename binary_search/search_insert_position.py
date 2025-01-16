b40f3fbc110b722561a87ad65cf7ff275d5df06c
Glebn5 <glebnowikoff@yandex.ru>
2025-01-16 18:48:24 +0300

    upd file name

---
+++ binary_search/search_insert_position.py
@@ -0,0 +1,16 @@
+def searchInsert(nums: list[int], target: int) -> int:
+    low = 0
+    high = len(nums) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = nums[mid]
+        if guess == target:
+            return mid
+        elif guess > target:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return low
+    
+
+print(searchInsert([1,3,5,6], 2))
