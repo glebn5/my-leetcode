c70a1cbe6b0f934182f767eed23d8aea97ca57f1
Glebn5 <glebnowikoff@yandex.ru>
2025-05-13 17:58:10 +0300

    upd count_negative_numbers_in_a_sorted_matrix.py

--- binary_search/count_negative_numbers_in_a_sorted_matrix.py
+++ binary_search/count_negative_numbers_in_a_sorted_matrix.py
@@ -1,21 +1,31 @@
 def countNegatives(grid: list[list[int]]) -> int:
     count = 0
-    for num in grid:
-        low = 0
-        high = len(num) - 1
-        for n in num:
-            if n < 0:
-                count += 1
-    #     while low <= high:
-    #         mid = (low + high) // 2
-    #         guess = num[mid]
-    #         if guess < 0:
-    #             count += high - 1
-    #             print(count, high)
-    #             high = mid - 1
-    #         else:
-    #             low = mid + 1
-    #         print('-----')
+    for row in grid:
+        low, high = 0, len(row) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if row[mid] < 0:
+                high = mid - 1
+            else:
+                low = mid + 1
+        
+        count += len(row) - low
     return count
 
-print(countNegatives([[-1, -2, -3], [-4, -5, -6],[-7, -8, -9]]))
+
+
+
+
+
+# def countNegatives(grid) -> int:
+#     count = 0
+#     for arr in grid:
+#         arr.sort()
+#         for i in arr:
+#             if i < 0:
+#                 count += 1
+#             else:
+#                 break
+#     return count
+
+print(countNegatives([[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]))
